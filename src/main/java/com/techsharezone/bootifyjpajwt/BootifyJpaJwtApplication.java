6a97ffe595f3b7efb04b9b5e49d906dc458dca85
Saurabh Sharma <saurabhshcs@gmail.com>
2020-11-22 00:54:27 +0000

    updated code base.

---
+++ src/main/java/com/techsharezone/bootifyjpajwt/BootifyJpaJwtApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootifyjpajwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifyJpaJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifyJpaJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsharezone/bootifyjpajwt/BootifyJpaJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootifyjpajwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifyJpaJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
